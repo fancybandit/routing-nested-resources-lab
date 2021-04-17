3aa1db3e6360d35aaa8e1e45534cb1a43c1aa99d
Jacob Klimenko <droidedjake@gmail.com>
2021-04-17 16:29:56 -0400

    commit tree 3f23f1b6ae0443a1cc53095a648efaeb209b7c0b

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,31 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.all
+    if !params[:artist_id].nil?
+      artist = Artist.find_by(id: params[:artist_id])
+      if artist.nil?
+        flash[:alert] = "Artist not found."
+        redirect_to artists_path
+      else
+        @songs = artist.songs
+      end
+    else
+      @songs = Song.all
+    end
   end
 
   def show
-    @song = Song.find(params[:id])
+    if params[:artist_id]
+      artist = Artist.find_by(id: params[:artist_id])
+      if artist.nil?
+        flash[:alert] = "Artist not found."
+        redirect_to artists_path
+      else
+        @song = artist.songs.find_by(id: params[:id])
+        redirect_to artist_songs_path(artist.id), alert: "Song not found." if @song.nil?
+      end
+    else
+      @song = Song.find_by(id: params[:id])
+    end
   end
 
   def new
@@ -47,7 +68,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :artist_id)
   end
 end
 
